11457d8a9b7f9b6f5b416d9398cf080c9ba4c093
vijayalakshmi <58000247+viar84@users.noreply.github.com>
2024-07-02 11:02:56 +0530

    Update DataBase_Utility.java

--- VTiger_Project/src/main/java/Generic_Utilities/DataBase_Utility.java
+++ VTiger_Project/src/main/java/Generic_Utilities/DataBase_Utility.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import com.mysql.jdbc.Driver;
-
+//database util
 public class DataBase_Utility
 {
 	public void databaseconnection() throws SQLException
